8a4fe104e0dd2fd10927b112792ec66f44b215ed
Brian Zahn <bzahn1@asu.edu>
2020-11-18 15:54:44 -0700

    further development of client model and clientinfo view

--- Sogeti_Client_Data_Repository/Models/Client.cs
+++ Sogeti_Client_Data_Repository/Models/Client.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Sogeti_Client_Data_Repository.Models
 {
@@ -11,5 +11,49 @@ namespace Sogeti_Client_Data_Repository.Models
     {
         public string ClientName { get; set; }
         public string Description { get; set; }
+
+        SqlConnection con;
+        public Client()
+        {
+            var config = GetConfiguration();
+            con = new SqlConnection(config.GetSection("Data").GetSection("ConnectionString").Value);
+        }
+
+        public IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddUserSecrets<Startup>()
+                .AddEnvironmentVariables();
+            return builder.Build();
+        }
+
+        public string GetClientInfo(Client client)
+        {
+            SqlCommand com = new SqlCommand("get_ClientInfo", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Name", client.ClientName);
+            com.Parameters.AddWithValue("@Description", client.Description);
+
+            int response;
+            try
+            {
+                using (con)
+                {
+                    con.Open();
+                    response = com.;
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return ;
+        }
     }
 }
