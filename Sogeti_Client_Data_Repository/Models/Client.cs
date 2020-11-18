7c3eca0ebe92747963c5a4dec4654d47e405a68c
Brian Zahn <bzahn1@asu.edu>
2020-11-17 22:45:59 -0700

    made requested changes to client info and client application pages, created client.cs model

---
+++ Sogeti_Client_Data_Repository/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sogeti_Client_Data_Repository.Models
+{
+    public class Client
+    {
+        public string ClientName { get; set; }
+        public string Description { get; set; }
+    }
+}
