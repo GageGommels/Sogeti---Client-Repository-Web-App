f7158d13b37d442cf94ddb2068a2a7cbafe7a95d
Brian Zahn <bzahn1@asu.edu>
2020-11-18 20:03:07 -0700

    created displayClient model class

--- Sogeti_Client_Data_Repository/Models/Client.cs
+++ Sogeti_Client_Data_Repository/Models/Client.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
-using System.Data.SqlClient;
-using System.Data;
-using System.Security.Cryptography;
-using System.Text;
-
+﻿
 namespace Sogeti_Client_Data_Repository.Models
 {
     public class Client
@@ -12,48 +6,5 @@ namespace Sogeti_Client_Data_Repository.Models
         public string ClientName { get; set; }
         public string Description { get; set; }
 
-        SqlConnection con;
-        public Client()
-        {
-            var config = GetConfiguration();
-            con = new SqlConnection(config.GetSection("Data").GetSection("ConnectionString").Value);
-        }
-
-        public IConfigurationRoot GetConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", true, true)
-                .AddUserSecrets<Startup>()
-                .AddEnvironmentVariables();
-            return builder.Build();
-        }
-
-        public string GetClientInfo(Client client)
-        {
-            SqlCommand com = new SqlCommand("get_ClientInfo", con);
-            com.CommandType = CommandType.StoredProcedure;
-            com.Parameters.AddWithValue("@Name", client.ClientName);
-            com.Parameters.AddWithValue("@Description", client.Description);
-
-            int response;
-            try
-            {
-                using (con)
-                {
-                    con.Open();
-                    response = com.;
-                }
-            }
-            catch (Exception e)
-            {
-                return e.Message;
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            return ;
-        }
     }
 }
---
+++ Sogeti_Client_Data_Repository/Models/displayClients.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sogeti_Client_Data_Repository.Models
+{
+    public class displayClients
+    {
+        SqlConnection con;
+        public displayClients()
+        {
+            var config = GetConfiguration();
+            con = new SqlConnection(config.GetSection("Data").GetSection("ConnectionString").Value);
+        }
+
+        public IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddUserSecrets<Startup>()
+                .AddEnvironmentVariables();
+            return builder.Build();
+        }
+
+        public string getClientInfo()
+        {
+            string data = "";
+
+            SqlCommand com = new SqlCommand("get_ClientInfo", con);
+            com.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                using (con)
+                {
+                    con.Open();
+                    using (SqlDataReader sqlReader = com.ExecuteReader())
+                    {
+                        if (sqlReader.HasRows)
+                        {
+                            while (sqlReader.Read())
+                            {
+                                int clientID = sqlReader.GetInt32(0);
+                                string name = sqlReader.GetString(1);
+                                string description = sqlReader.GetString(2);
+
+                                data += "<tr><td scope='col' class='col-3'><a href='ClientApplications/'>" + name + "</a></td><td scope='col' class='col-9'>" + description + "</td></tr>";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return data;
+        }
+
+        public string getClientSearchInfo(Client client)
+        {
+            string data = "";
+
+            SqlCommand com = new SqlCommand("get_ClientSearchInfo", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Name", client.ClientName);
+            com.Parameters.AddWithValue("@Description", client.Description);
+
+            try
+            {
+                using (con)
+                {
+                    con.Open();
+                    using (SqlDataReader sqlReader = com.ExecuteReader())
+                    {
+                        if (sqlReader.HasRows)
+                        {
+                            while (sqlReader.Read())
+                            {
+                                int clientID = sqlReader.GetInt32(0);
+                                string name = sqlReader.GetString(1);
+                                string description = sqlReader.GetString(2);
+
+                                data += "<tr><td scope='col' class='col-3'><a href='ClientApplications/'>" + name + "</a></td><td scope='col' class='col-9'>" + description + "</td></tr>";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return data;
+        }
+    }
+}
